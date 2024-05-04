4e14b85fda5c3a256dd9d5c2526d21a02738b87f
dhairya9622 <dhairya9622@gmail.com>
2024-05-04 13:34:10 -0700

    Initial commit

---
+++ src/app/recipes/recipe-detail/recipe-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-details.component.html',
+  styleUrl: './recipe-details.component.css'
+})
+export class RecipeDetailsComponent {
+
+}
---
+++ src/app/recipes/recipe-list/recipelist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipelist.component.html',
+  styleUrl: './recipelist.component.css'
+})
+export class RecipelistComponent {
+
+}
